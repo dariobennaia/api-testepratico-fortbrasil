0c7cfd06517f7ac05f8188158d9a8d31fe80dc4f
Dario Santos <dario.santos@mobtelecom.com.br>
2020-01-23 23:21:47 -0300

    Finalizando comentarios nos arquivos da aplicaçao

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,8 +1,15 @@
+/** **************************************************************
+ * ***************************************************************
+ * **                                                           **
+ * **                  Rotas da aplicação.                      **
+ * **                                                           **
+ * ***************************************************************
+ * ************************************************************ */
 const { Router } = require('express');
 
 const routes = Router();
 
-const IsAuthenticated = require('../middlewares/IsAuthenticated');
+const auth = require('../middlewares/IsAuthenticated');
 const ShopController = require('../controllers/ShopController');
 const SearchShopController = require('../controllers/SearchShopController');
 const UserController = require('../controllers/UserController');
@@ -13,40 +20,46 @@ const updateUserValidation = require('../validations/user/UserUpdateValidator');
 const createShopValidation = require('../validations/shop/ShopCreateValidator');
 const updateShopValidation = require('../validations/shop/ShopCreateValidator');
 const loginValidation = require('../validations/auth/LoginValidator');
-const changePasswordValidation = require('../validations/auth/ChangePasswordValidator');
+const changePassValidaton = require('../validations/auth/ChangePasswordValidator');
 
+/** **************************************************************
+ * ***************************************************************
+ * **                                                           **
+ * **        Rotas para o gerênciamento de autenticação.        **
+ * **                                                           **
+ * ***************************************************************
+ * ************************************************************ */
 routes.post('/login', loginValidation, AuthController.store);
-routes.patch(
-  '/change-password',
-  changePasswordValidation,
-  AuthController.update
-);
-routes.delete('/logout', IsAuthenticated, AuthController.delete);
+routes.patch('/change-password', changePassValidaton, AuthController.update);
+routes.delete('/logout', auth, AuthController.delete);
+/* ***************************************************************
+ * ************************************************************ */
 
+/** **************************************************************
+ * ***************************************************************
+ * **                                                           **
+ * **        Rotas para o gerênciamento de usuários.            **
+ * **                                                           **
+ * ***************************************************************
+ * ************************************************************ */
 routes.post('/users', createUserValidation, UserController.store);
+routes.post('/users/:id', auth, updateUserValidation, UserController.update);
+/* ***************************************************************
+ * ************************************************************ */
 
-routes.post(
-  '/users/:id',
-  IsAuthenticated,
-  updateUserValidation,
-  UserController.update
-);
-
-routes.get('/shops', IsAuthenticated, ShopController.index);
-routes.post(
-  '/shops',
-  IsAuthenticated,
-  createShopValidation,
-  ShopController.store
-);
-routes.patch(
-  '/shops/:id',
-  IsAuthenticated,
-  updateShopValidation,
-  ShopController.update
-);
-routes.delete('/shops/:id', IsAuthenticated, ShopController.delete);
-
+/** **************************************************************
+ * ***************************************************************
+ * **                                                           **
+ * **       Rotas para o gerênciamento de autenticação.         **
+ * **                                                           **
+ * ***************************************************************
+ * ************************************************************ */
+routes.get('/shops', auth, ShopController.index);
+routes.post('/shops', auth, createShopValidation, ShopController.store);
+routes.patch('/shops/:id', auth, updateShopValidation, ShopController.update);
+routes.delete('/shops/:id', auth, ShopController.delete);
 routes.get('/shops/distance-of/:distance', SearchShopController.index);
+/* ***************************************************************
+ * ************************************************************ */
 
 module.exports = routes;
